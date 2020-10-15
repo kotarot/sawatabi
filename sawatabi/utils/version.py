caf94751b912f0d1dcac5bbff793bc7985815a3a
Kotaro Terada <kotarot@apache.org>
2020-10-15 23:43:04 +0900

    Separate version.py and move version func to utils (#7)
    * Separate version.py and move version func to utils
    
    * Bump up version to 0.0.1.dev1

--- sawatabi/utils/version.py
+++ sawatabi/utils/version.py
@@ -12,4 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.0.1.dev1'
+from ..version import __version__
+
+def version():
+    return __version__
