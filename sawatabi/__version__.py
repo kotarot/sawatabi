36f5d29364f3ce5d635ca1a54e30a6ce14715220
Kotaro Terada <kotarot@apache.org>
2020-10-16 01:19:26 +0900

    Bump up version to 0.0.1.dev2

--- sawatabi/__version__.py
+++ sawatabi/__version__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.0.1.dev1'
+__version__ = '0.0.1.dev2'
 __version_info__ = tuple(int(i) if i.isdigit() else i for i in __version__.split('.'))
