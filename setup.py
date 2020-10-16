8438130027103094b57be4391a9a05cae0506db7
Kotaro Terada <kotarot@apache.org>
2020-10-17 00:21:55 +0900

    Try adding Python 3.7

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
     classifiers=[
         "Development Status :: 1 - Planning",
         "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
 )
