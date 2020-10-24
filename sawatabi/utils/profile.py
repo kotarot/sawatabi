434f8f2549de2d6809ed232cf8d3c04be8585b8d
Kotaro Terada <kotarot@apache.org>
2020-10-25 00:43:33 +0900

    Deal with isort

--- sawatabi/utils/profile.py
+++ sawatabi/utils/profile.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from functools import wraps
 import time
+from functools import wraps
 
 
 def profile(func):
--- tests/test_profile.py
+++ tests/test_profile.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
 import random
+
+import pytest
 
 from sawatabi.model import LogicalModel
 from sawatabi.utils.profile import profile
