856618a2c5d6f512f85439cacec9e0d735a36c16
Kotaro Terada <kotarot@apache.org>
2020-10-25 22:18:08 +0900

    Relax timing limit of local solver for GitHub Actions

--- tests/test_local_solver.py
+++ tests/test_local_solver.py
@@ -109,9 +109,9 @@ def test_local_solver_n_hot_ising(n, s):
     assert np.count_nonzero(result == 1) == n
     assert np.count_nonzero(result == -1) == s - n
 
-    # Execution time should be within 1 sec.
-    assert resultset.info["timing"]["elapsed_sec"] <= 1.0
-    assert resultset.info["timing"]["elapsed_counter"] <= 1.0
+    # Execution time should be within 5 sec.
+    assert resultset.info["timing"]["elapsed_sec"] <= 5.0
+    assert resultset.info["timing"]["elapsed_counter"] <= 5.0
 
 
 @pytest.mark.parametrize("n,s", [(1, 2), (1, 3), (2, 3), (1, 4), (2, 4), (1, 100), (10, 400)])
@@ -128,9 +128,9 @@ def test_local_solver_n_hot_qubo(n, s):
     assert np.count_nonzero(result == 1) == n
     assert np.count_nonzero(result == 0) == s - n
 
-    # Execution time should be within 1 sec.
-    assert resultset.info["timing"]["elapsed_sec"] <= 1.0
-    assert resultset.info["timing"]["elapsed_counter"] <= 1.0
+    # Execution time should be within 5 sec...
+    assert resultset.info["timing"]["elapsed_sec"] <= 5.0
+    assert resultset.info["timing"]["elapsed_counter"] <= 5.0
 
 
 def test_local_solver_with_logical_model_fails():
