a29644e02cc4698e6ba2d73c18b32f844acbbf57
Kotaro Terada <kotarot@apache.org>
2020-12-21 01:58:15 +0900

    Add tests for current_time

--- tests/utils/test_current_time.py
+++ tests/utils/test_current_time.py
@@ -19,17 +19,35 @@ import numpy
 import sawatabi
 
 
-def test_current_time():
+def test_current_time_type():
+    assert isinstance(sawatabi.utils.current_time(), float)
+    assert isinstance(sawatabi.utils.current_time_ms(), float)
+    assert isinstance(sawatabi.utils.current_time_us(), float)
+    assert isinstance(sawatabi.utils.current_time_ns(), float)
+
+    assert isinstance(sawatabi.utils.current_time_as_int(), int)
+    assert isinstance(sawatabi.utils.current_time_ms_as_int(), int)
+    assert isinstance(sawatabi.utils.current_time_us_as_int(), int)
+    assert isinstance(sawatabi.utils.current_time_ns_as_int(), int)
+
+
+def test_current_time_value():
     current_time = time.time()
 
     # Check the time approximately
 
     # sec
     numpy.testing.assert_approx_equal(actual=sawatabi.utils.current_time(), desired=current_time, significant=9)
+    numpy.testing.assert_approx_equal(actual=sawatabi.utils.current_time_as_int(), desired=current_time, significant=9)
 
     # milli sec
     numpy.testing.assert_approx_equal(
         actual=sawatabi.utils.current_time_ms(),
+        desired=current_time * 1_000,
+        significant=9,
+    )
+    numpy.testing.assert_approx_equal(
+        actual=sawatabi.utils.current_time_ms_as_int(),
         desired=current_time * 1_000,
         significant=9,
     )
@@ -40,10 +58,20 @@ def test_current_time():
         desired=current_time * 1_000_000,
         significant=9,
     )
+    numpy.testing.assert_approx_equal(
+        actual=sawatabi.utils.current_time_us_as_int(),
+        desired=current_time * 1_000_000,
+        significant=9,
+    )
 
     # nano sec
     numpy.testing.assert_approx_equal(
         actual=sawatabi.utils.current_time_ns(),
+        desired=current_time * 1_000_000_000,
+        significant=9,
+    )
+    numpy.testing.assert_approx_equal(
+        actual=sawatabi.utils.current_time_ns_as_int(),
         desired=current_time * 1_000_000_000,
         significant=9,
     )
