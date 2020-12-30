f9d643409a965bda973752153d434eae8df3d763
Kotaro Terada <kotarot@apache.org>
2020-12-30 21:40:28 +0900

    Deal with isort

--- tests/algorithm/test_io.py
+++ tests/algorithm/test_io.py
@@ -12,9 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
-
 import apache_beam as beam
+import pytest
 
 from sawatabi.algorithm.io import IO
 
