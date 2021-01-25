af014e5eafe8c4132dfe73971fef46f55793b419
Kotaro Terada <kotarot@apache.org>
2021-01-25 23:21:28 +0900

    Add a test

--- tests/solver/test_dwave_solver.py
+++ tests/solver/test_dwave_solver.py
@@ -80,6 +80,23 @@ def test_dwave_solver_with_auth_parameters(mocker, physical):
     assert isinstance(resultset, dimod.SampleSet)
 
 
+def test_dwave_solver_with_embedding_parameters(mocker, physical):
+    solver = DWaveSolver(endpoint="http://0.0.0.0/method")
+
+    sampleset = dimod.SampleSet.from_samples([{"x[0]": 1, "x[1]": -1}], dimod.SPIN, energy=[-2.0])
+    sampleset._info = {
+        "timing": {"qpu_sampling_time": 12345},
+        "problem_id": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+    }
+
+    mocker.patch("dwave.system.samplers.DWaveSampler.__init__", return_value=None)
+    mocker.patch("dwave.system.composites.EmbeddingComposite.__init__", return_value=None)
+    mocker.patch("dwave.system.composites.EmbeddingComposite.sample", return_value=sampleset)
+    resultset = solver.solve(physical, embedding_parameters={"random_seed": 12345})
+
+    assert isinstance(resultset, dimod.SampleSet)
+
+
 def test_dwave_default_solver_name():
     solver = DWaveSolver()
     assert solver._solver == "Advantage_system1.1"
