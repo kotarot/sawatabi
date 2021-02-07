645a03b315385a875e2766fee983651283bb03e2
Kotaro Terada <kotarot@apache.org>
2021-02-07 13:44:28 +0900

    Fix a syntax error

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setup(
             "black>=20.8b1,<21.0",
             "flake8>=3.8.4,<4.0.0",
             "isort>=5.6.4,<6.0.0",
+        ],
         "sample": [
             # For samples
             "geopy>=2.0.0,<3.0.0",
